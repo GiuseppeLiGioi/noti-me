3a14f75e801c57cbb52bb0e35a6ea8d52f1d1ae8
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-11-07 20:44:39 +0100

    started. structure of HomePage and base style

--- src/Screens/HomeScreen.tsx
+++ src/Screens/HomeScreen.tsx
@@ -1,8 +1,12 @@
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
+import { useState } from "react";
 
 
 export default function HomeScreen() {
+
+    
 
 
     const styles = StyleSheet.create({
@@ -36,9 +40,43 @@ export default function HomeScreen() {
             textAlign: "center",
             marginTop: 20,
             maxWidth: "70%"
-        }
-    })
+        },
 
+        buttonTextTopHome:{
+            color: "blue"
+        },
+
+        containerEmojiHome:{
+            flexDirection: "row",
+            gap: 40,
+
+        },
+
+        containerBottomHome: {
+            justifyContent: "center",
+            alignContent: "center",
+            gap: 20,
+            padding: 40,
+            borderRadius: 15,
+            backgroundColor: "black",
+            textAlign: "center",
+            borderWidth: 1,
+            marginTop: 30,
+
+            borderColor: "rgba(239, 251, 213, 1)"
+        },
+
+        emojiText : {
+            textAlign: "center",
+            fontSize: 24,
+            color: "white"
+        }
+
+
+
+    })
+    
+    const [emoji, setEmoji] = useState <'happy' | 'neutral' | 'sad' | null> (null)
 
     return (
 
@@ -50,7 +88,30 @@ export default function HomeScreen() {
 
             <Text style={styles.title}>Benvenuto su NotiMe!</Text>
             <Text style={styles.semiTitle}>Il mio primo piccolo progetto in React Native + TypeScript.</Text>
-            <TouchableOpacity style={styles.button}><Text>Vai alle note!</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button}><Text style={styles.buttonTextTopHome}>Vai alle note!</Text></TouchableOpacity>
+
+            <View style={styles.containerBottomHome}>
+                <Text style={styles.emojiText}>Come ti senti oggi!?</Text>
+
+                <View style={styles.containerEmojiHome}>
+                    <TouchableOpacity  onPress={() => setEmoji('happy')}>
+
+                    <FontAwesome5 name = "smile" size={70} color={emoji === "happy" ? "green" : "gray"}/>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => setEmoji('neutral')}>
+
+                    <FontAwesome5 name = "meh" size={70} color={emoji === "neutral" ? "yellow" : "gray"}/>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => setEmoji('sad')}>
+
+                    <FontAwesome5 name = "frown" size={70} color={emoji === "sad" ? "red" : "gray"}/>
+                    </TouchableOpacity>
+
+
+                </View>
+            </View>
         </LinearGradient>
 
     )
